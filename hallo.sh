4ef3010ef714921ebc7aaba47d5560e8a764a985
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2023-12-20 13:52:28 +0100

    Update hallo.sh

--- hallo.sh
+++ hallo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Hallo Welt $1"
 curl "http://google.com"
-sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+sudo apt-get install chromium-browser
