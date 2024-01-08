27ca1e4a6c41744b7952c3fa6ced3b2056013eff
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-08 10:59:33 +0100

    Update hallo.sh

--- hallo.sh
+++ hallo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Hallo Welt $1"
 curl "http://google.com"
+echo "Chromium wird installiert"
 sudo snap install chromium chromium-ffmpeg
+echo "Chromium wurde installiert"
+echo "Visual Studio Code wird installiert"
+wget -q -O "-" https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+sudo apt-get install codium
+echo "Visual Studio Code wurde installiert"
+echo "Geany wird installiert"
+sudo apt-get install geany
+echo "Geany wurde installiert"
