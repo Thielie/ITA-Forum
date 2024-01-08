371d2dd6f830b3c219b9eddf1c0b61a17e4d7d45
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-08 11:13:11 +0100

    Update hallo.sh

--- hallo.sh
+++ hallo.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+# Farbdefinitionen
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 echo "Hallo Welt $1"
 curl "http://google.com"
-echo "Aktualisiere das System..."
+echo "{YELLOW}Aktualisiere das System...{NC}"
 sudo apt update && sudo apt upgrade -y
-echo "Installiere Chromium..."
+echo "{GREEN}Das System wurde aktualisiert!{NC}"
+echo "{YELLOW}Installiere Chromium...{NC}"
 sudo apt install -y chromium-browser
-echo "Installiere Visual Studio Code..."
+echo "{GREEN}Chromium wurde installiert{NC}"
+echo "{YELLOW}Installiere Visual Studio Code...{NC}"
 sudo snap install --classic code
-echo "Installiere Geany..."
+echo "{GREEN}Visual Studio Code wurde installiert{NC}"
+echo "{YELLOW}Installiere Geany...{NC}"
 sudo apt install -y geany
+echo "{GREEN}Geany wurde installiert{NC}"
 echo "Installation abgeschlossen."
