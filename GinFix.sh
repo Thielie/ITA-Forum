316d4543d8dc5c73de3c2be1320b891d8c65f85b
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-09 12:05:41 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -4,51 +4,50 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-#Update von Ubuntu#
+# Update von Ubuntu
 echo -e "${YELLOW}Aktualisiere das System...${NC}"
 sudo apt update && sudo apt upgrade -y
 echo -e "${GREEN}Das System wurde erfolgreich aktualisiert!${NC}"
 
-#Chromium Installation#
+# Chromium Installation
 echo -e "${YELLOW}Installiere Chromium...${NC}"
 sudo apt install -y chromium-browser
 echo -e "${GREEN}Chromium wurde erfolgreich installiert!${NC}"
 
-#Visual Studio Code Installation#
+# Visual Studio Code Installation
 echo -e "${YELLOW}Installiere Visual Studio Code...${NC}"
 sudo snap install --classic code
 echo -e "${GREEN}Visual Studio Code wurde erfolgreich installiert!${NC}"
 
-#Geany Installation#
+# Geany Installation
 echo -e "${YELLOW}Installiere Geany...${NC}"
 sudo apt install -y geany
 echo -e "${GREEN}Geany wurde erfolgreich installiert!${NC}"
 
-#LAMP-Stack Installation#
-#Apache Server installation 
+# LAMP-Stack Installation
+# Apache Server installation
 echo -e "${YELLOW}Apache-Server wird installiert...${NC}"
 sudo apt install -y apache2
 echo -e "${GREEN}Apache-Server wurde erfolgreich installiert!${NC}"
 sudo ufw enable
 sudo ufw allow in "Apache"
 
-#MySql-Server installation
+# MySQL-Server installation
 echo -e "${YELLOW}MySQL-Server wird installiert...${NC}"
-mysql_root_password=""
 sudo apt install -y mysql-server
-sudo service mysql start
 sudo systemctl enable mysql
-echo -e "${GREEN}Der MySql-Server wurde erfolgreich installiert!${NC}"
-#PHP-Paket wird installiert
+echo -e "${GREEN}Der MySQL-Server wurde erfolgreich installiert!${NC}"
+
+# PHP-Paket wird installiert
 echo -e "${YELLOW}PHP-Paket wird installiert...${NC}"
-sudo apt install php libapache2-mod-php php-mysql
+sudo apt install -y php libapache2-mod-php php-mysql
 echo -e "${GREEN}PHP-Paket wurde erfolgreich installiert!${NC}"
 echo -e "${YELLOW}PHP-Paket wird nach der Version überprüft...${NC}"
 php -v
 echo -e "${GREEN}PHP-Paket wurde erfolgreich nach der Version überprüft!${NC}"
 
-#WordPress Installation#
-#WordPress-CLI herunterladen und installieren
+# WordPress Installation
+# WordPress-CLI herunterladen und installieren
 echo -e "${YELLOW}Der WordPress-Client wird installiert...${NC}"
 sudo apt-get install -y wp-cli
 echo -e "${GREEN}Der WordPress-Client wurde erfolgreich installiert!${NC}"
@@ -75,8 +74,8 @@ NEW_USER_EMAIL="cit@example.com"
 echo -e "${GREEN}Der neue Benutzer wurde erfolgreich konfiguriert!${NC}"
 
 # Verzeichnis für WordPress-Installation
-echo -e "${YELLOW}Die WordPress installation wird in das ${WP_INSTALL_DIR} Verzeichnis gelegt...${NC}"
 WP_INSTALL_DIR="/var/www/html"
+echo -e "${YELLOW}Die WordPress-Installation wird in das ${WP_INSTALL_DIR} Verzeichnis gelegt...${NC}"
 echo -e "${GREEN}WordPress wurde erfolgreich in das ${WP_INSTALL_DIR} Verzeichnis gelegt!${NC}"
 
 # WordPress herunterladen und entpacken
@@ -84,30 +83,30 @@ echo -e "${YELLOW}WordPress wird heruntergeladen und entpackt...${NC}"
 wget -c https://wordpress.org/${WP_VERSION}.tar.gz
 tar -xzvf ${WP_VERSION}.tar.gz -C /tmp/
 rm ${WP_VERSION}.tar.gz
-cp -R /tmp/wordpress/* ${WP_INSTALL_DIR}
-rm -rf /tmp/wordpress
+sudo cp -R /tmp/wordpress/* ${WP_INSTALL_DIR}
+sudo rm -rf /tmp/wordpress
 echo -e "${GREEN}WordPress wurde erfolgreich heruntergeladen und entpackt!${NC}"
 
 # WordPress-Konfigurationsdatei erstellen
 echo -e "${YELLOW}Eine WordPress-Konfigurationsdatei wird erstellt...${NC}"
-cp ${WP_INSTALL_DIR}/wp-config-sample.php ${WP_INSTALL_DIR}/wp-config.php
-sed -i "s/wordpress/$WP_DB_NAME/" ${WP_INSTALL_DIR}/wp-config.php
-sed -i "s/cit/$WP_DB_USER/" ${WP_INSTALL_DIR}/wp-config.php
-sed -i "s/cit/$WP_DB_PASSWORD/" ${WP_INSTALL_DIR}/wp-config.php
-sed -i "s/localhost/$WP_DB_HOST/" ${WP_INSTALL_DIR}/wp-config.php
-sed -i "s/wp_/$WP_TABLE_PREFIX/" ${WP_INSTALL_DIR}/wp-config.php
+sudo cp ${WP_INSTALL_DIR}/wp-config-sample.php ${WP_INSTALL_DIR}/wp-config.php
+sudo sed -i "s/wordpress/$WP_DB_NAME/" ${WP_INSTALL_DIR}/wp-config.php
+sudo sed -i "s/cit/$WP_DB_USER/" ${WP_INSTALL_DIR}/wp-config.php
+sudo sed -i "s/cit/$WP_DB_PASSWORD/" ${WP_INSTALL_DIR}/wp-config.php
+sudo sed -i "s/localhost/$WP_DB_HOST/" ${WP_INSTALL_DIR}/wp-config.php
+sudo sed -i "s/wp_/$WP_TABLE_PREFIX/" ${WP_INSTALL_DIR}/wp-config.php
 echo -e "${GREEN}Die WordPress-Konfigurationsdatei wurde erfolgreich erstellt!${NC}"
 
 # WordPress-Verzeichnis-Berechtigungen setzen
 echo -e "${YELLOW}Berechtigungen für das ${WP_INSTALL_DIR} Verzeichnis werden konfiguriert...${NC}"
-chown -R www-data:www-data ${WP_INSTALL_DIR}
-chmod -R 777 ${WP_INSTALL_DIR}
+sudo chown -R www-data:www-data ${WP_INSTALL_DIR}
+sudo chmod -R 755 ${WP_INSTALL_DIR}
 echo -e "${GREEN}Berechtigungen für das ${WP_INSTALL_DIR} Verzeichnis wurden erfolgreich konfiguriert!${NC}"
 
 # Apache-Konfiguration
 echo -e "${YELLOW}Apache wird konfiguriert...${NC}"
-a2enmod rewrite
-service apache2 restart
+sudo a2enmod rewrite
+sudo service apache2 restart
 echo -e "${GREEN}Apache wurde erfolgreich konfiguriert und neu gestartet!${NC}"
 
 # WordPress installieren
@@ -120,11 +119,8 @@ echo -e "${YELLOW}Der neue Benutzer wird hinzugefügt...${NC}"
 wp user create $NEW_USER $NEW_USER_EMAIL --user_pass=$NEW_USER_PASSWORD --display_name=$NEW_USER --role=subscriber --path="${WP_INSTALL_DIR}"
 echo -e "${GREEN}Der neue Benutzer wurde erfolgreich hinzugefügt!${NC}"
 
-# Erfolgsmeldung ausgeben
-echo -e "${GREEN}WordPress wurde erfolgreich installiert!${NC}"
-
-#PhpMyAdmin installation und konfiguration'
-#Konfigurationsvariablen
+# PhpMyAdmin Installation und Konfiguration
+# Konfigurationsvariablen
 echo -e "${YELLOW}Konfigurationsvariablen werden erstellt...${NC}"
 DB_USER="cit"
 DB_USER_PASSWORD="cit"
@@ -133,31 +129,29 @@ APACHE_CONF_DIR="/etc/apache2/conf-available"
 APACHE_CONF_FILE="phpmyadmin.conf"
 echo -e "${GREEN}Konfigurationsvariablen wurden erfolgreich erstellt!${NC}"
 
-#PhpMyAdmin installieren
+# PhpMyAdmin installieren
 echo -e "${YELLOW}PhpMyAdmin wird installiert...${NC}"
-apt-get install -y phpmyadmin
+sudo apt-get install -y phpmyadmin
 echo -e "${GREEN}PhpMyAdmin wurde erfolgreich installiert!${NC}"
 
-#MySql-Benutzer für PhpMyAdmin konfigurieren
-echo -e "${YELLOW}MySql-Benutzer wird für PhpMyAdmin konfiguriert...${NC}"
+# MySQL-Benutzer für PhpMyAdmin konfigurieren
+echo -e "${YELLOW}MySQL-Benutzer wird für PhpMyAdmin konfiguriert...${NC}"
 mysql -u root -p <<MYSQL_SCRIPT
 CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON . TO '${DB_USER}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
-echo -e "${GREEN}MySql-Benutzer wurde erfolgreich für PhpMyAdmin konfiguriert!${NC}"
+echo -e "${GREEN}MySQL-Benutzer wurde erfolgreich für PhpMyAdmin konfiguriert!${NC}"
 
-#PhpMyAdmin Konfiguration für Apache erstellen
+# PhpMyAdmin Konfiguration für Apache erstellen
 echo -e "${YELLOW}PhpMyAdmin Konfiguration wird für Apache erstellt...${NC}"
-echo "Include $PHPMYADMIN_DIR/apache.conf" > "$APACHE_CONF_DIR/$APACHE_CONF_FILE"
-a2enconf "$APACHE_CONF_FILE"
+sudo echo "Include $PHPMYADMIN_DIR/apache.conf" > "$APACHE_CONF_DIR/$APACHE_CONF_FILE"
+sudo a2enconf "$APACHE_CONF_FILE"
 echo -e "${GREEN}PhpMyAdmin Konfiguration wurde erfolgreich für Apache erstellt!${NC}"
 
-#Apache-Server neu starten
+# Apache-Server neu starten
 echo -e "${YELLOW}Apache-Server wird neu gestartet...${NC}"
 sudo service apache2 restart
 echo -e "${GREEN}Apache-Server wurde erfolgreich neu gestartet!${NC}"
-echo -e "${GREEN}phpMyAdmin wurde erfolgreich installiert und konfiguriert. MySQL-Benutzer 'ita' wurde erstellt!${NC}"
 
-echo "Installation abgeschlossen."
-
+echo -e "${GREEN}Die gesamte Installation wurde erfolgreich abgeschlossen!${NC}"
