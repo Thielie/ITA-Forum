3cebfdd526b4cd5c666ed1ac6591504382f92c6d
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-10 08:13:14 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -62,80 +62,64 @@ php -v
 echo -e "${GREEN}PHP-Paket wurde erfolgreich nach der Version überprüft!${NC}"
 
 # WordPress Installation
-# WordPress-CLI herunterladen und installieren
-echo -e "${YELLOW}Installiere wp-cli...${NC}"
-sudo apt-get install -y curl
-sudo -u www-data curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-sudo chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-echo -e "${GREEN}wp-cli wurde erfolgreich installiert!${NC}"
+# Datenbankkonfiguration
+DB_NAME="wordpress"
+DB_USER="admin"
+DB_PASSWORD="admin"
+DB_HOST="localhost"
 
 # WordPress Konfiguration
-echo -e "${YELLOW}WordPress wird konfiguriert...${NC}"
-WP_VERSION="latest"
-WP_DB_NAME="wordpress"
-WP_DB_USER="root"
-WP_DB_PASSWORD=""
-WP_DB_HOST="localhost"
-WP_TABLE_PREFIX="wp_"
-WP_SITE_URL="localhost"
+WP_DIR="/var/www/html"
+WP_URL="http://localhost"
+WP_TITLE="My WordPress Site"
 WP_ADMIN_USER="admin"
 WP_ADMIN_PASSWORD="admin"
 WP_ADMIN_EMAIL="admin@example.com"
-echo -e "${GREEN}WordPress wurde erfolgreich konfiguriert!${NC}"
 
-# Neuer Benutzer Konfiguration
-echo -e "${YELLOW}Ein neuer Benutzer wird konfiguriert...${NC}"
-NEW_USER="cit"
-NEW_USER_PASSWORD="cit"
-NEW_USER_EMAIL="cit@example.com"
-echo -e "${GREEN}Der neue Benutzer wurde erfolgreich konfiguriert!${NC}"
-
-# Verzeichnis für WordPress-Installation
-WP_INSTALL_DIR="/var/www/html"
-echo -e "${YELLOW}Die WordPress-Installation wird in das ${WP_INSTALL_DIR} Verzeichnis gelegt...${NC}"
-echo -e "${GREEN}WordPress wurde erfolgreich in das ${WP_INSTALL_DIR} Verzeichnis gelegt!${NC}"
+# Neuen Benutzer für WordPress erstellen
+WP_USER="cit"
+WP_USER_PASSWORD="cit"
 
 # WordPress herunterladen und entpacken
 echo -e "${YELLOW}WordPress wird heruntergeladen und entpackt...${NC}"
-wget -c https://wordpress.org/${WP_VERSION}.tar.gz
-tar -xzvf ${WP_VERSION}.tar.gz -C /tmp/
-rm ${WP_VERSION}.tar.gz
-sudo cp -R /tmp/wordpress/* ${WP_INSTALL_DIR}
-sudo rm -rf /tmp/wordpress
+wget -c https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz -C /tmp/
+sudo cp -R /tmp/wordpress/* $WP_DIR
+sudo chown -R www-data:www-data $WP_DIR
+sudo chmod -R 777 $WP_DIR
+rm latest.tar.gz
 echo -e "${GREEN}WordPress wurde erfolgreich heruntergeladen und entpackt!${NC}"
 
 # WordPress-Konfigurationsdatei erstellen
-echo -e "${YELLOW}Eine WordPress-Konfigurationsdatei wird erstellt...${NC}"
-sudo cp ${WP_INSTALL_DIR}/wp-config-sample.php ${WP_INSTALL_DIR}/wp-config.php
-sudo sed -i "s/wordpress/$WP_DB_NAME/" ${WP_INSTALL_DIR}/wp-config.php
-sudo sed -i "s/cit/$WP_DB_USER/" ${WP_INSTALL_DIR}/wp-config.php
-sudo sed -i "s/cit/$WP_DB_PASSWORD/" ${WP_INSTALL_DIR}/wp-config.php
-sudo sed -i "s/127.0.0.1/$WP_DB_HOST/" ${WP_INSTALL_DIR}/wp-config.php
-sudo sed -i "s/wp_/$WP_TABLE_PREFIX/" ${WP_INSTALL_DIR}/wp-config.php
-echo -e "${GREEN}Die WordPress-Konfigurationsdatei wurde erfolgreich erstellt!${NC}"
+echo -e "${YELLOW}WordPress-Konfigurationsdatei wird erstellt...${NC}"
+sudo cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
+sudo sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
+sudo sed -i "s/username_here/$DB_USER/" $WP_DIR/wp-config.php
+sudo sed -i "s/password_here/$DB_PASSWORD/" $WP_DIR/wp-config.php
+sudo sed -i "s/localhost/$DB_HOST/" $WP_DIR/wp-config.php
+echo -e "${GREEN}WordPress-Konfigurationsdatei wurde erfolgreich erstellt!${NC}"
 
-# WordPress-Verzeichnis-Berechtigungen setzen
-echo -e "${YELLOW}Berechtigungen für das ${WP_INSTALL_DIR} Verzeichnis werden konfiguriert...${NC}"
-sudo chown -R www-data:www-data ${WP_INSTALL_DIR}
-sudo chmod -R 777 ${WP_INSTALL_DIR}
-echo -e "${GREEN}Berechtigungen für das ${WP_INSTALL_DIR} Verzeichnis wurden erfolgreich konfiguriert!${NC}"
-
-# Apache-Konfiguration
-echo -e "${YELLOW}Apache wird konfiguriert...${NC}"
+# Apache-Konfiguration für mod_rewrite aktivieren
+echo -e "${YELLOW}Aktiviere mod_rewrite in Apache...${NC}"
 sudo a2enmod rewrite
 sudo service apache2 restart
-echo -e "${GREEN}Apache wurde erfolgreich konfiguriert und neu gestartet!${NC}"
+echo -e "${GREEN}mod_rewrite wurde erfolgreich aktiviert und Apache wurde neu gestartet!${NC}"
 
-# WordPress installieren
-echo -e "${YELLOW}WordPress wird installiert...${NC}"
-wp core install --url="$WP_SITE_URL" --title="My WordPress Site" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="${WP_INSTALL_DIR}"
-echo -e "${GREEN}WordPress wurde erfolgreich installiert!${NC}"
+# WordPress über die Befehlszeile installieren
+echo -e "${YELLOW}WordPress wird über die Befehlszeile installiert...${NC}"
+sudo -u www-data wp core install \
+    --url="$WP_URL" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --path="$WP_DIR"
+echo -e "${GREEN}WordPress wurde erfolgreich über die Befehlszeile installiert!${NC}"
 
-# Neuen Benutzer hinzufügen
-echo -e "${YELLOW}Der neue Benutzer wird hinzugefügt...${NC}"
-wp user create $NEW_USER $NEW_USER_EMAIL --user_pass=$NEW_USER_PASSWORD --display_name=$NEW_USER --role=subscriber --path="${WP_INSTALL_DIR}"
-echo -e "${GREEN}Der neue Benutzer wurde erfolgreich hinzugefügt!${NC}"
+# Neuen Benutzer für WordPress erstellen
+echo -e "${YELLOW}Neuen Benutzer wird erstellt...${NC}"
+sudo -u www-data wp user create $WP_USER $WP_USER_PASSWORD --role=author --path="$WP_DIR"
+echo -e "${GREEN}Neuer Benutzer wurde erfolgreich erstellt!${NC}"
 
 #PHPmyAdmin Installation
 # MySQL-Benutzer für phpMyAdmin konfigurieren
@@ -156,7 +140,7 @@ echo -e "${GREEN}phpMyAdmin wurde erfolgreich installiert!${NC}"
 # PhpMyAdmin-Konfiguration für Apache erstellen
 PHPMYADMIN_CONF_FILE="/etc/apache2/conf-available/phpmyadmin.conf"
 echo -e "${YELLOW}PhpMyAdmin Konfiguration wird für Apache erstellt...${NC}"
-echo "Include /etc/phpmyadmin/apache.conf" | run_sudo tee -a $PHPMYADMIN_CONF_FILE
+#echo "Include /etc/phpmyadmin/apache.conf" | run_sudo tee -a $PHPMYADMIN_CONF_FILE
 sudo a2enconf phpmyadmin
 sudo service apache2 restart
 echo -e "${GREEN}PhpMyAdmin Konfiguration wurde erfolgreich für Apache erstellt!${NC}"
