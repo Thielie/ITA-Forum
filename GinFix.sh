d465281fe7ed777a3be54432b4224ae3ca3cdc81
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-11 18:23:24 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -61,17 +61,19 @@ MYSQL_ROOT_PASSWORD="root"
 
 DB_USER="cit"
 DB_PASSWORD="cit"
+
+# MySQL-Benutzer für phpMyAdmin konfigurieren
 echo -e "${YELLOW}MySQL-Benutzer wird für phpMyAdmin konfiguriert...${NC}"
 sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
 CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;
-REVOKE ALL PRIVILEGES ON sys.* FROM 'cit'@'localhost';
-REVOKE ALL PRIVILEGES ON phpmyadmin.* FROM 'cit'@'localhost';
-REVOKE ALL PRIVILEGES ON mysql.* FROM 'cit'@'localhost';
-REVOKE ALL PRIVILEGES ON information_schema.* FROM 'cit'@'localhost';
-REVOKE ALL PRIVILEGES ON performance_schema.* FROM 'cit'@'localhost';
+GRANT ALL PRIVILEGES ON ${WORDPRESS_DB}.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;
+REVOKE ALL PRIVILEGES ON sys.* FROM '${DB_USER}'@'localhost';
+REVOKE ALL PRIVILEGES ON mysql.* FROM '${DB_USER}'@'localhost';
+REVOKE ALL PRIVILEGES ON phpmyadmin.* FROM '${DB_USER}'@'localhost';
+REVOKE ALL PRIVILEGES ON information_schema.* FROM '${DB_USER}'@'localhost';
+REVOKE ALL PRIVILEGES ON performance_schema.* FROM '${DB_USER}'@'localhost';
+FLUSH PRIVILEGES;
 MYSQL_SCRIPT
-echo -e "${GREEN}MySQL-Benutzer wurde erfolgreich für phpMyAdmin konfiguriert!${NC}"
 
 # Erlaube MySQL-Root-Anmeldung über Socket-Mechanismus
 echo -e "${YELLOW}Erlaube MySQL-Root-Anmeldung über Socket...${NC}"
