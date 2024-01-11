337ad08ef93c21dabb2c27a32a9ca352d621071f
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-11 09:23:45 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# config.sh
+export MYSQL_ROOT_PASSWORD="root"
+export DB_USER="cit"
+export DB_PASSWORD="cit"
+export WP_ADMIN_USER="admin"
+export WP_ADMIN_PASSWORD="admin"
+export WP_ADMIN_EMAIL="admin@example.com"
+export WP_USER="cit"
+export WP_USER_EMAIL="cit@example.com"
+export WP_USER_PASSWORD="cit"
