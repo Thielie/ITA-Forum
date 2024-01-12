d4322ed815160d773ed25771471a4627f71693b6
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-12 11:50:46 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -70,9 +70,11 @@ CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';
 FLUSH PRIVILEGES;
 
-$(for db in "${EXCLUDED_DATABASES[@]}"; do
+SQL_QUERY=$(for db in "${EXCLUDED_DATABASES[@]}"; do
     echo "REVOKE ALL PRIVILEGES ON $db.* FROM '$DB_USER'@'localhost';"
-done) | sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}"
+done)
+
+echo "${SQL_QUERY}" | sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}"
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
