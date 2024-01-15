6937d572c47f2c077edaaea551c61d07089cd9f5
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-15 13:32:14 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -67,13 +67,13 @@ MYSQL_ROOT_PASSWORD="root"
 DB_USER="cit"
 DB_PASSWORD="cit"
 #EXCLUDED_DATABASES=("sys" "mysql" "phpmyadmin" "information_schema" "performance_schema")
-#GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost';
+
 
 # MySQL-Benutzer erstellen und Berechtigungen setzen
-echo -e "${YELLOW}MySQL-Benutzer wird für phpMyAdmin konfiguriert... Der User ${DB_USER} Bekommt alle Rechte.${NC}"
+echo -e "${YELLOW}MySQL-Benutzer wird für phpMyAdmin konfiguriert...${NC}"
 mysql -u root -p$MYSQL_ROOT_PASSWORD <<MYSQL_SCRIPT
 CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
