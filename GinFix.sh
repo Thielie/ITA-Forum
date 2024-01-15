d0c346c4814ef9e9d492bc43f37e4c917a8f1c73
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-15 13:33:56 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -72,19 +72,19 @@ DB_PASSWORD="cit"
 # MySQL-Benutzer erstellen und Berechtigungen setzen
 echo -e "${YELLOW}MySQL-Benutzer wird für phpMyAdmin konfiguriert...${NC}"
 mysql -u root -p$MYSQL_ROOT_PASSWORD <<MYSQL_SCRIPT
-CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost';
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}'
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost'
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 # Root-Benutzer entzieht Zugriff auf Systemdatenbanken
 mysql -u root -p$MYSQL_ROOT_PASSWORD <<MYSQL_REVOKE_SCRIPT
-REVOKE ALL PRIVILEGES ON mysql.* FROM '${DB_USER}'@'localhost';
-REVOKE ALL PRIVILEGES ON performance_schema.* FROM '${DB_USER}'@'localhost';
-REVOKE ALL PRIVILEGES ON information_schema.* FROM '${DB_USER}'@'localhost';
-REVOKE ALL PRIVILEGES ON sys.* FROM '${DB_USER}'@'localhost';
-REVOKE ALL PRIVILEGES ON phpmyadmin.* FROM '${DB_USER}'@'localhost';
-FLUSH PRIVILEGES;
+REVOKE ALL PRIVILEGES ON mysql.* FROM '${DB_USER}'@'localhost'
+REVOKE ALL PRIVILEGES ON performance_schema.* FROM '${DB_USER}'@'localhost'
+REVOKE ALL PRIVILEGES ON information_schema.* FROM '${DB_USER}'@'localhost'
+REVOKE ALL PRIVILEGES ON sys.* FROM '${DB_USER}'@'localhost'
+REVOKE ALL PRIVILEGES ON phpmyadmin.* FROM '${DB_USER}'@'localhost'
+FLUSH PRIVILEGES
 EXIT;
 MYSQL_REVOKE_SCRIPT
 
