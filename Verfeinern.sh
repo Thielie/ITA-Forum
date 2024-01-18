b45e8aae1b13978983b3770286cd1fc6a442f04a
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-18 09:03:59 +0100

    Update Verfeinern.sh

--- Verfeinern.sh
+++ Verfeinern.sh
@@ -27,6 +27,9 @@ error_message() {
     echo -e "${RED}Fehler bei der Installation von $1.${NC}"
 }
 
+# MySQL-Root-Passwort
+MYSQL_ROOT_PASSWORD="root"
+
 # Update von Ubuntu
 echo -e "${YELLOW}Aktualisiere das System...${NC}"
 if sudo apt update && sudo apt upgrade -y; then
@@ -73,7 +76,7 @@ else
 fi
 
 # LAMP-Stack Installation
-Apache Server installation
+# Apache Server installation
 echo -e "${YELLOW}Apache-Server wird installiert...${NC}"
 sudo apt install -y apache2
 echo -e "${GREEN}Apache-Server wurde erfolgreich installiert!${NC}"
@@ -108,13 +111,13 @@ FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 echo -e "${GREEN}MySQL-Root-Anmeldung über Socket wurde erfolgreich aktiviert!${NC}"
 
-MYSQL_ROOT_PASSWORD="root"
+# MySQL-Benutzer für phpMyAdmin konfigurieren
 DB_USER="cit"
 DB_PASSWORD="cit"
 EXCLUDED_DATABASES=("sys" "mysql" "phpmyadmin" "information_schema" "performance_schema")
 
 echo -e "${YELLOW}MySQL-Benutzer wird für phpMyAdmin konfiguriert...${NC}"
-#sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
+sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
 CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
@@ -157,107 +160,85 @@ if [ "$(ask_user 'WordPress')" == "j" ]; then
     ask_user_and_password
     WP_USER_PASSWORD_HASH=$(openssl passwd -1 $password)
 
-
-# WordPress herunterladen und entpacken
-echo -e "${YELLOW}WordPress wird heruntergeladen und entpackt...${NC}"
-wget -c https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz -C /tmp/
-sudo mkdir -p $WP_DIR  # Erstellt den Ordner "wp"
-sudo cp -R /tmp/wordpress/* $WP_DIR
-sudo chown -R www-data:www-data $WP_DIR
-sudo chmod -R 777 $WP_DIR
-rm latest.tar.gz
- if [ $? -eq 0 ]; then
+    # WordPress herunterladen und entpacken
+    echo -e "${YELLOW}WordPress wird heruntergeladen und entpackt...${NC}"
+    wget -c https://wordpress.org/latest.tar.gz
+    tar -xzvf latest.tar.gz -C /tmp/
+    sudo mkdir -p $WP_DIR  # Erstellt den Ordner "wp"
+    sudo cp -R /tmp/wordpress/* $WP_DIR
+    sudo chown -R www-data:www-data $WP_DIR
+    sudo chmod -R 777 $WP_DIR
+    rm latest.tar.gz
+    if [ $? -eq 0 ]; then
         success_message "WordPress (Herunterladen und Entpacken)"
     else
         error_message "WordPress (Herunterladen und Entpacken)"
     fi
 
-# MySQL-Root-Passwort
-MYSQL_ROOT_PASSWORD="root"
-
-# Apache-Konfiguration für mod_rewrite aktivieren
-echo -e "${YELLOW}Aktiviere mod_rewrite in Apache...${NC}"
-sudo a2enmod rewrite
-sudo systemctl restart apache2
-echo -e "${GREEN}mod_rewrite wurde erfolgreich aktiviert und Apache wurde neu gestartet!${NC}"
-
-# WP-CLI installieren
-echo "Installing WP-CLI..."
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-
-# MySQL-Datenbank erstellen
-echo -e "${YELLOW}MySQL-Datenbank wird erstellt...${NC}"
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
+    # MySQL-Datenbank erstellen
+    echo -e "${YELLOW}MySQL-Datenbank wird erstellt...${NC}"
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 MYSQL_SCRIPT
- if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         success_message "MySQL-Datenbank"
     else
         error_message "MySQL-Datenbank"
     fi
 
+    # MySQL-Benutzer für WordPress erstellen
+    WP_DB_USER="wordpress"
+    WP_DB_PASSWORD="wordpress"
 
-# MySQL-Root-Passwort
-MYSQL_ROOT_PASSWORD="root"
-
-# MySQL-Benutzer für WordPress erstellen
-WP_DB_USER="wordpress"
-WP_DB_PASSWORD="wordpress"
-
-echo -e "${YELLOW}MySQL-Benutzer 'wordpress' wird erstellt...${NC}"
-sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
+    echo -e "${YELLOW}MySQL-Benutzer 'wordpress' wird erstellt...${NC}"
+    sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
 CREATE USER '${WP_DB_USER}'@'localhost' IDENTIFIED BY '${WP_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${WP_DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
-  if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         success_message "MySQL-Benutzer '$WP_DB_USER'"
     else
         error_message "MySQL-Benutzer '$WP_DB_USER'"
     fi
-echo -e "${GREEN}MySQL-Benutzer 'wordpress' wurde erfolgreich erstellt!${NC}"
 
-# WordPress Installation mit manuell erstellter wp-config.php
-echo -e "${YELLOW}WordPress wird in der Datenbank '$DB_NAME' installiert...${NC}"
+    # WordPress Installation mit manuell erstellter wp-config.php
+    echo -e "${YELLOW}WordPress wird in der Datenbank '$DB_NAME' installiert...${NC}"
 
-# Manuell wp-config.php erstellen
-sudo cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
-sudo sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
-sudo sed -i "s/username_here/$WP_DB_USER/" $WP_DIR/wp-config.php
-sudo sed -i "s/password_here/$WP_DB_PASSWORD/" $WP_DIR/wp-config.php
-sudo sed -i "s/localhost/$DB_HOST/" $WP_DIR/wp-config.php
+    # Manuell wp-config.php erstellen
+    sudo cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
+    sudo sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
+    sudo sed -i "s/username_here/$WP_DB_USER/" $WP_DIR/wp-config.php
+    sudo sed -i "s/password_here/$WP_DB_PASSWORD/" $WP_DIR/wp-config.php
+    sudo sed -i "s/localhost/$DB_HOST/" $WP_DIR/wp-config.php
 
- if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         success_message "WordPress (Installation)"
     else
         error_message "WordPress (Installation)"
     fi
 
-# WordPress-Datenbank erstellen
-sudo -u www-data wp core install \
-    --url="$WP_URL" \
-    --title="$WP_TITLE" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_password="$WP_ADMIN_PASSWORD" \
-    --admin_email="$WP_ADMIN_EMAIL" \
-    --path="$WP_DIR" \
-    --skip-email
+    # WordPress-Datenbank erstellen
+    sudo -u www-data wp core install \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path="$WP_DIR" \
+        --skip-email
 
-# Überprüfen Sie, ob die Installation erfolgreich war
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}WordPress wurde erfolgreich in der Datenbank '$DB_NAME' über die Befehlszeile installiert!${NC}"
-else
-    echo -e "${RED}Fehler bei der WordPress-Installation.${NC}"
-fi
+    # Überprüfen Sie, ob die Installation erfolgreich war
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}WordPress wurde erfolgreich in der Datenbank '$DB_NAME' über die Befehlszeile installiert!${NC}"
+    else
+        echo -e "${RED}Fehler bei der WordPress-Installation.${NC}"
+    fi
 
-# Neuen Benutzer für WordPress erstellen
-echo -e "${YELLOW}Neuer Benutzer wird erstellt...${NC}"
-sudo -u www-data wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --role=administrator --path="$WP_DIR"
-echo -e "${GREEN}Neuer Benutzer wurde erfolgreich erstellt!${NC}"
- if [ $? -eq 0 ]; then
+    # Neuen Benutzer für WordPress erstellen
+    echo -e "${YELLOW}Neuer Benutzer wird erstellt...${NC}"
+    sudo -u www-data wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --role=administrator --path="$WP_DIR"
+    if [ $? -eq 0 ]; then
         success_message "Neuer Benutzer für WordPress"
     else
         error_message "Neuer Benutzer für WordPress"
