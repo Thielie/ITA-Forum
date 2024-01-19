1753da9f5604da1f2217163e4a5fde36a1664a93
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-19 10:27:49 +0100

    Update Verfeinern.sh

--- Verfeinern.sh
+++ Verfeinern.sh
@@ -20,10 +20,9 @@ error_message() {
 MYSQL_ROOT_PASSWORD="root"
 
 # Benutzerabfrage für Software-Installationen
-read -n 1 -p "${FAT}$(tput setaf 12)Möchtest du Chromium installieren? (j/N):$(tput sgr0) " chromium_choice < /dev/tty
-read -n 1 -p "${FAT}$(tput setaf 12)Möchtest du Visual Studio Code installieren? (j/N):$(tput sgr0) " vs_code_choice < /dev/tty
-read -n 1 -p "${FAT}$(tput setaf 12)Möchtest du Geany installieren? (j/N):$(tput sgr0) " geany_choice < /dev/tty
-echo ""
+read -p "${FAT}$(tput setaf 12)Möchtest du Chromium installieren? (j/n):$(tput sgr0) " chromium_choice < /dev/tty
+read -p "${FAT}$(tput setaf 12)Möchtest du Visual Studio Code installieren? (j/n):$(tput sgr0) " vs_code_choice < /dev/tty
+read -p "${FAT}$(tput setaf 12)Möchtest du Geany installieren? (j/n):$(tput sgr0) " geany_choice < /dev/tty
 
 # Update von Ubuntu
 echo -e "${FAT}${YELLOW}Aktualisiere das System...${NC}$(tput sgr0)"
