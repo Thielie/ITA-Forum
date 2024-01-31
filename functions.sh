c11540dc888a80ffc8b290646200f688e7737d0f
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-31 14:29:10 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -27,31 +27,9 @@ get_user_choice() {
 
 # Überprüfen, ob curl installiert ist
 if ! command -v curl &> /dev/null; then
-    echo "$(tput bold)$(tput setaf 1)curl ist nicht installiert. Bitte installieren Sie curl, um fortzufahren. Benutzen Sie dafür folgenden Befehl: sudo apt-get install curl$(tput sgr0)"
+    echo "$(tput bold)$(tput setaf 1)Fehler: curl ist nicht installiert. Bitte installieren Sie curl, um fortzufahren. Verwenden Sie dafür folgenden Befehl: sudo apt-get install curl. Gib danach folgenden Begel ein: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
     exit 1
 fi
-
-# Überprüfen, ob das Skript mit curl von GitHub ausgeführt wird
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-    # Das Skript wird nicht direkt ausgeführt (z.B. mit source oder als Teil eines anderen Skripts)
-    echo "$(tput bold)$(tput setaf 1)Das Skript sollte mit dem Befehl 'curl' von GitHub ausgeführt werden. Bitte folgenden Befehl benutzen: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
-    exit 1
-fi
-
-# Überprüfen, ob die Aufrufzeile 'curl' enthält
-if [[ $(ps -o comm= $PPID) != "curl" ]]; then
-    echo "$(tput bold)$(tput setaf 1)Das Skript sollte mit dem Befehl 'curl' von GitHub ausgeführt werden. Bitte folgenden Befehl benutzen: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
-    exit 1
-fi
-
-# Überprüfen, ob das Skript von GitHub geholt wird (basiert auf der Annahme, dass die URL im Skript statisch ist)
-if grep -q "https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh" "${BASH_SOURCE[0]}"; then
-    echo "Das Skript wird mit curl von GitHub ausgeführt."
-else
-    echo "$(tput bold)$(tput setaf 1)Das Skript sollte mit dem Befehl 'curl' von GitHub ausgeführt werden. Bitte folgenden Befehl benutzen: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
-    exit 1
-fi
-
 
 blink_text() {
     local text="$1"
