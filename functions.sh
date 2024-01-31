4c201225c9b63b585c16b57d09b7814fdbb93eed
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-01-31 15:36:50 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -27,7 +27,7 @@ get_user_choice() {
 
 # Überprüfen, ob curl installiert ist
 if ! command -v curl &> /dev/null; then
-    echo "$(tput bold)$(tput setaf 1)Fehler: curl ist nicht installiert. Bitte installieren Sie curl, um fortzufahren. Verwenden Sie dafür folgenden Befehl: sudo apt-get install curl. Gib danach folgenden Begel ein: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
+    echo -e "$(tput bold)$(tput setaf 1)Fehler: curl ist nicht installiert. Bitte installieren Sie curl, um fortzufahren. Verwenden Sie dafür folgenden Befehl: sudo apt-get install curl. Gib danach folgenden Begel ein: curl -L https://raw.githubusercontent.com/Thielie/ITA-Forum/MW3/GinFix.sh | bash$(tput sgr0)"
     exit 1
 fi
 
