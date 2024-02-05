950c1bb5dc4f435f3b9d2c3b7252dd05bc984714
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-02-05 10:06:36 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,6 +5,15 @@ MYSQL_ROOT_PASSWORD="root"
 FILE="/etc/apt/preferences.d/nosnap.pref"
 DB_USER="cit"
 DB_PASSWORD="cit"
+# WordPress Installation
+# Datenbankkonfiguration
+DB_NAME="wordpress"
+DB_HOST="localhost"
+# WordPress Konfiguration
+html="/var/www/html"
+WP_DIR="/var/www/html/wp"  # Hier wird der Ordner "wp" erstellt
+WP_URL="http://localhost/wp"
+WP_TITLE="My WordPress Site"
 WP_ADMIN_USER="admin"
 WP_ADMIN_PASSWORD="admin"
 WP_ADMIN_EMAIL="admin@example.com"
