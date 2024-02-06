d85f336c8a7fb588107eb9ad4dedd63f4fdc2bd6
Rasierkoffer <145977095+Rasierkoffer@users.noreply.github.com>
2024-02-06 08:11:58 +0100

    Update GinFix.sh

--- GinFix.sh
+++ GinFix.sh
@@ -352,8 +352,6 @@ else
     echo -e "${FAT}${RED}Fehler beim Aktualisieren der Berechtigungen!${NC}${NF}"
 fi
 
-sudo chmod 444 /var/www/html/error.log
-
 #Link zum html Ordner auf den Desktop
 ln -s /var/www/html $HOME/Schreibtisch/html
 
